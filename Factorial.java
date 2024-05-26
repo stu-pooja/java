3e2931886f8c429c0258ad82b19097e420e60c7d
stu-pooja <pooja0933518@gmail.com>
2024-05-26 22:14:19 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,11 +1,11 @@
 public class Factorial {
 
     public static void main(String[] args) {
-        int num=4;
-        int factorial=1;
+        int num=10;
+        int fact=1;
         for(int i=1;i<=num;i++){
            factorial *=i;
         }
-        System.out.println("factorial : "+factorial);
+        System.out.println("factorial is: "+fact);
     }
 }
